0a3b44d6ce9c3607a0bc03856be54eaaff9d12a8
robiwankenobi <rfnkurle@gmail.com>
2020-06-16 16:54:00 -0600

    arrow functions

--- wes bos/ways-to-make-a-function.js
+++ wes bos/ways-to-make-a-function.js
@@ -1,1 +1,34 @@
 console.log("it works")
+
+// function doctorize(){
+//     return `Dr. ${firstName}`
+// }
+
+
+//Anonymous function
+// function (firstName) {
+//     return `Dr. ${firstName}`
+// }
+
+//Function Expression, putting a function inside a variable
+//functions in a variables are not hoisted
+
+// const doctorize = function (firstName) {
+//     return `Dr. ${firstName}`
+// }
+
+
+//arrow functions are anonymous functions
+//one line, own scope
+
+//explicit return
+const inchToCM = function(inches){
+    const cm = inches * 2.54;
+    //return inches * 2.54;
+    return cm;
+};
+
+//implicit return
+const inchToCM = (inches0 => inches * 2.54;
+
+

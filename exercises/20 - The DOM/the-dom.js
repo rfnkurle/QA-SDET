0f0f8ae60861baca2121f1351e1abd1f54ede2fc
robiwankenobi <rfnkurle@gmail.com>
2020-06-18 21:09:39 -0600

    new folders

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,33 @@
+console.log('it works');
+
+const p = document.querySelector('p')
+const img = document.querySelectorAll(".items img");
+// const item2 =document.querySelector("item2")
+// const item2image = item2.querySelector('img')
+// console.log(img)
+
+const heading = document.querySelector('h2')
+heading.textContent = "Robby is Cool"
+console.dir(heading.textContent)
+
+
+
+
+
+
+
+
+
+
+// const nice = document.querySelector(".nice")
+// nice.classList.add('open')
+// nice.classList.remove('something')
+
+// console.log(nice.classList)
+
+
+// function toggleClick(){
+//     nice.classList.toggle("round")
+// }
+
+// nice.addEventListener("click", toggleClick)
